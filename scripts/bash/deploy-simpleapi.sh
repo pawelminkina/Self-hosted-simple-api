984a5fc443e322e4d8b37999d689bfbad3de0c60
pavixonpl <pawmin14@gmail.com>
2024-05-11 22:40:45 +0200

    change

--- scripts/bash/deploy-simpleapi.sh
+++ scripts/bash/deploy-simpleapi.sh
@@ -1,5 +1,5 @@
 #!usr/bin/env
-#has to be added in directory /home/pavixon/automation
+#has to be added in directory /home/pavixon/automation with chmod +x
 #crontab -u pavixon -e
 #* * * * * /home/pavixon/automation/deploy-simpleapi.sh
 #sudo nano filename can open text editor
@@ -8,4 +8,4 @@ export DOCKER_CONTENT=pavixon
 
 LOCK_FILE="${pwd}/simple-api.lock"
 cd /home/pavixon/services/Self-hosted-simple-api
-flock -n $LOCK_FILE ./scripts/deploy-if-changed.sh >> /home/pavixon/automation/deploy-simpleapi.log 2>&1 
+flock -n $LOCK_FILE ./scripts/bash/deploy-if-changed.sh >> /home/pavixon/automation/deploy-simpleapi.log 2>&1 
