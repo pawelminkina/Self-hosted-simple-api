890839a833f838ac73fa75b5850f986029bad1de
pavixonpl <pawmin14@gmail.com>
2024-05-12 00:51:32 +0200

    g

--- scripts/bash/deploy.sh
+++ scripts/bash/deploy.sh
@@ -5,19 +5,19 @@ cd src
 
 BUILD_VERSION=$(git rev-parse HEAD)
 
-echo "$(date --utc +% FT%TZ): Releasing new server version. $BUILD_VERSION"
+echo "$(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
 
-echo "$(date --utc +% FT%TZ): Running build..."
+echo "$(date --utc +%FT%TZ): Running build..."
 docker compose rm -f
 docker compose build
 
 OLD_CONTAINER=$(docker ps -aqf "name=server")
-echo "$(date --utc +% FT%TZ): Scaling server up..."
+echo "$(date --utc +%FT%TZ): Scaling server up..."
 BUILD_VERSION=$BUILD_VERSION docker compose up -d --no-deps --scale server=2 --no-recreate server
 
 sleep 30
 
-echo "$(date --utc +% FT%TZ): Scaling old server down..."
+echo "$(date --utc +%FT%TZ): Scaling old server down..."
 docker container rm -f $OLD_CONTAINER
 docker compose up -d --no-deps --scale server=1 --no-recreate server
 
